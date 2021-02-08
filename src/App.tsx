f0e1a8b42e5ab0e843654eb353830b1385666305
Francico Campos <54690739+flcj@users.noreply.github.com>
2021-02-07 19:44:00 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit This took way too long for just a simple error.
+          This took way too long for just a simple error.
         </p>
         <a
           className="App-link"
